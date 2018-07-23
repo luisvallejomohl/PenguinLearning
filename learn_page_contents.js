8a79ebcd90ca7897f15881157517cbaecf8f8bcd
luisvallejomohl <38414346+luisvallejomohl@users.noreply.github.com>
2018-07-24 07:52:25 +1200

    Update learn_page_contents.js

--- learn_page_contents.js
+++ learn_page_contents.js
@@ -2,6 +2,7 @@
 function learn(){
 	var _middle = PenguinsJS('middle');
 	var $middle = jQuery(_middle.toDOMNodeList[0]);
+	
 	$middle.html('');/*Clear the contents of the learn page*/	
 	/*_middle.addElement('h1', 0, 'learnHeading').set('innerHTML', 'Learn');
 	_middle.addElement('h2', 0, 'mathHeading').set('innerHTML', 'Math');
@@ -11,20 +12,25 @@ function learn(){
 	.set('className', 'qwvxxe')
 	.set('innerHTML', 'Solving equations')
 	.set('id', ''); //Add the link to the Solving Equations playlist*/
+	
 	_middle.addElement('h2', 0, 'computingHeading').set('innerHTML', 'Computing');
+	
 	_middle.addElement('a', 0, 'htmlCodingLink').set('className', 'qwvxxe')
-	.set('href', 'file:///C:/Users/Luis/Documents/Desktop/M/PenguinLearning/index.html/#htmlCoding')
-	.set('innerHTML', 'HTML Live editor')
-	.onEvent('click', function(){location.reload()});
+	.set('href', '#htmlCoding')
+	.set('innerHTML', 'HTML Live editor');
+	
 	_middle.addElement('br', 1);
+	
 	_middle.addElement('a', 0, 'makingWebpagesLink').set('className', 'qwvxxe')
-	.set('href', 'file:///C:/Users/Luis/Documents/Desktop/M/PenguinLearning/index.html/#makingWebpages')
+	.set('href', '#makingWebpages')
 	.set('innerHTML', 'HTML & CSS:Making webpages');
+	
+	_middle.addElement('br', 1);
+	
 	_middle.addElement('a', 0, 'jsEditorLink')
-	.set('className', 'qwvxxe').set('href', 'file:///C:/Users/Luis/Documents/Desktop/M/PenguinLearning/index.html/#jsCoding')
+	.set('className', 'qwvxxe').set('#jsCoding')
 	.set('innerHTML', 'JS Live editor');
 };
 if(location.hash == '#learn'){
 	learn();
 };
-$('a[href="file:///C:/Users/Luis/Documents/Desktop/M/PenguinLearning/index.html/#learn"]').on('click', learn);
