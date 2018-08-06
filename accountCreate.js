29de0e4c4912e9b1758a8987a5eb5a7ae8d83966
luisvallejomohl <38414346+luisvallejomohl@users.noreply.github.com>
2018-08-06 16:19:48 +1200

    Update accountCreate.js

--- accountCreate.js
+++ accountCreate.js
@@ -1,2 +1,2 @@
-var createAccount = new Function(M[0] + "$('#accountCreate')[0].innerHTML += 'Your user ID is ' + String(eval(localStorage.getItem('users')).length - 1) + '. You will need this to log in and to recover your username or password if you forget them. <STRONG>Do <EM>NOT</EM> give it away!!!</STRONG>'");
+var createAccount = new Function(M[1] + "$('#accountCreate')[0].innerHTML += 'Your user ID is ' + String(eval(localStorage.getItem('users')).length - 1) + '. You will need this to log in and to recover your username or password if you forget them. <STRONG>Do <EM>NOT</EM> give it away!!!</STRONG>'");
 $('#accountCreate button').on('click', createAccount);
