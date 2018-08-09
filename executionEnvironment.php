b546c0ac68826b53aeeeb7993294da82e61c88b0
luisvallejomohl <38414346+luisvallejomohl@users.noreply.github.com>
2018-08-09 17:25:12 +1200

    Create executionEnvironment.php

---
+++ executionEnvironment.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<META CHARSET="ISO-8859-1">
+		<TITLE><?php
+		echo 'This title was chosen by PHP!';
+		?></TITLE>
+	</HEAD>
+	<BODY>
+<?php 
+echo('This text was made with PHP!');
+?>
+	</BODY>
+</HTML>
