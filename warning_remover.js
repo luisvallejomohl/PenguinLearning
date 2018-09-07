047f8cadf0f0135e2dcc40d954dc7b9475b70198
luisvallejomohl <38414346+luisvallejomohl@users.noreply.github.com>
2018-09-08 07:09:05 +1200

    Delete warning_remover.js

--- warning_remover.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('javaScript').style.display = 'none';/*Make the 'Please enable javaScript' message invisible*/
-if(typeof PenguinsJS != 'undefined' && typeof jQuery != 'undefined' && typeof _ != 'undefined'){/*If all the JS Libraries are available, */
-	__('#libraries').set('outerHTML', '');/*Make the 'We are having trouble loading javaScript libraries' message invisible*/
-};
-if(typeof localStorage !== 'undefined' && typeof sessionStorage !== 'undefined'){
-	document.getElementById('localStorage').style.display = 'none';/*Make the 'You cannot log on using this browser' message invisible*/
-};
